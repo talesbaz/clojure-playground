d3b8dd6e1624f1c4493eb4555f5c7f66c3a26b24
talesbaz <talesbaz@gmail.com>
2019-12-26 12:53:29 -0300

    add content

---
+++ vector.clj
@@ -0,0 +1,10 @@
+(def metallica-song 
+  (vector "Fuel" "Seek and Destroy" 30 "Metal Militia"))
+
+(get metallica-song 0) 
+(get metallica-song 2)
+(last metallica-song) 
+
+(conj metallica-song "St. Anger")
+
+metallica-song
